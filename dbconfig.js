7df673aa99859d66318f33f7ba2a33e57e1ed135
DHRUV KHANNA <dhruvkhannastar@gmail.com>
2021-12-05 14:41:05 +0530

    db connection using NodeJS

---
+++ dbconfig.js
@@ -0,0 +1,15 @@
+const config = {
+    user:'dhruv',
+    password:'talent',
+    server: '127.0.0.1',
+    database:'Products',
+    trustServerCertificate: true,
+    options:{
+        trustedconnection: true,
+        enableArithAort: true,
+        instancename: 'DESKTOP-LH2EQF1'
+    },
+    port: 1433
+}
+
+module.exports = config;
